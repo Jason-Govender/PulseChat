ef8bfbe723750b04c9e2ad74812283a52835f74f
Jason Govender <Jason.govender@boxfusion.io>
2026-02-11 12:52:18 +0200

    Implemented Registration and Login Features

---
+++ js/landing-page.js
@@ -0,0 +1,25 @@
+document.getElementById("login-form").addEventListener("submit", handleLogin);
+
+//Takes in user input, checks against the localStorage user array, validates and redirects to home on success
+function handleLogin(event) {
+    event.preventDefault();
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    
+    const validUser = users.find(user => user.username === username && user.password === password);
+
+    if(validUser) {
+        sessionStorage.setItem(
+            "currentUser", JSON.stringify({
+                username: validUser.username
+            })
+        );
+
+        location.replace("pages/home.html");
+    } else {
+        alert("Username and/or Password is Invalid");
+    }
+
+}
+
---
+++ js/sign-up.js
@@ -0,0 +1,28 @@
+document.getElementById("registration-form").addEventListener("submit", handleRegister);
+
+//Takes in user input, checks the username in localstorage. If none exists, add user to localstorage and redirect to home.html
+function handleRegister(event){
+    event.preventDefault();
+    const username = document.getElementById("username").value.trim();
+    const password = document.getElementById("password").value.trim();
+
+    const users = JSON.parse(localStorage.getItem("users")) || [];
+    const userExists = users.some(user => user.username === username);
+
+    if(userExists){
+        alert("Username Has Been Taken");
+        return;
+    }
+
+    const newUser = {
+        username: username,
+        password: password
+    };
+
+    const updatedUsers = [...users, newUser];
+    localStorage.setItem("users", JSON.stringify(updatedUsers));
+
+    location.replace("home.html");
+}
+
+
